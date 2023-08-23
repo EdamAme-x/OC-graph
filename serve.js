1a70945ab6529e1a35b3872d2bd965180c349de0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-23 19:38:36 +0900

    a

---
+++ serve.js
@@ -0,0 +1,5 @@
+import { serve } from "https://deno.land/std@0.141.0/http/mod.ts";
+import { serveDir } from "https://deno.land/std@0.141.0/http/file_server.ts";
+serve((request) => {
+    return serveDir(request);
+});
